344746ca9e2fa1965eaf9cbb0d59e2ae44700975
vongoctinh <tinhremix2000@gmail.com>
2020-08-21 00:48:59 +0700

    project new

---
+++ funstion.php
@@ -0,0 +1,14 @@
+<?php
+<a>Tạo project</a>
+// git init
+// git add
+// git commit -m "(mô tả )"
+// git push
+<a>đưa code lên</a>
+// git add .
+// git commit -m "(mô tả)"
+// git push
+<a>lấy code về</a>
+//git clone (đường link)
+// git pull (dùng trước khi push code lên)
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+<a href="">Anh Tính dẹp trai vãi lozz, Hãy thả tym cho anh ấy</a>
+<a>tôi đả them dòng này vào ngày 20/8/2020sss</a>
+
+?>
